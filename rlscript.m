5f028023677259601d52774ef59678076c7cffd9
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-03-09 20:49:07 +0530

    Working training script

--- rlscript.m
+++ rlscript.m
@@ -1,17 +1,33 @@
-% 1. Define the Observation Space (State: q1, q2, dq1, dq2) (4x1)
+%% 1. Defining the Environment Boundaries
+% Observation Space (State: q1, q2, dq1, dq2)
 obsInfo = rlNumericSpec([4 1]);
 obsInfo.Name = 'ArmStates';
 
-% 2. Define the Action Space (Torque: tau1, tau2) (2x1)
-% physical limits
+% Action Space (Torque: tau1, tau2) bounded to +/- 10 Nm
 actInfo = rlNumericSpec([2 1], 'LowerLimit', -10, 'UpperLimit', 10);
 actInfo.Name = 'JointTorques';
 
-% 3. bind model to env
 mdl = 'kinovagen3'; 
-blk = [mdl, '/RL Agent']; %path to agent
-
-% Construct the environment
+blk = [mdl, '/RL Agent']; 
 env = rlSimulinkEnv(mdl, blk, obsInfo, actInfo);
 
-env.ResetFcn = @(in) setVariable(in, 'seed', randi(100));
+%% 2. Constructing the SAC Agent
+agentOpts = rlSACAgentOptions;
+agentOpts.SampleTime = 0.1; 
+agentOpts.DiscountFactor = 0.99; 
+agentOpts.ExperienceBufferLength = 1e6; 
+
+agent = rlSACAgent(obsInfo, actInfo, agentOpts);
+
+%% 3. Executing the Training Loop
+trainOpts = rlTrainingOptions(...
+    'MaxEpisodes', 500, ...               
+    'MaxStepsPerEpisode', 200, ...        
+    'ScoreAveragingWindowLength', 50, ... 
+    'Verbose', false, ...
+    'Plots', 'training-progress', ...     
+    'StopTrainingCriteria', 'AverageReward', ...
+    'StopTrainingValue', -10);            
+
+% Launching the solver
+trainingStats = train(agent, env, trainOpts);
